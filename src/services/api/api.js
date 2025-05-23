b35dd3c496f84c192596ac3fdfb2c149ad52abdc
alexbeldam <alexbeldam@gmail.com>
2025-05-23 16:35:20 -0300

    feat: request header

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
+import useAuthStore from "../../stores/auth";
 
 const baseURL = `${import.meta.env.VITE_BACKEND_URL}`;
 
 const api = axios.create({ baseURL });
+
+api.interceptors.request.use(
+  (req) => {
+    const { token } = useAuthStore.getState();
+
+    if (!req.headers.Authorization && token) req.headers.Authorization = `Bearer ${token}`;
+
+    return req;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
