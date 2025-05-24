145117f8470aeac5184de7b464cacef37d52e5ed
alexbeldam <alexbeldam@gmail.com>
2025-05-24 01:32:21 -0300

    feat: image carousel

--- src/pages/Home/Styles.js
+++ src/pages/Home/Styles.js
@@ -6,3 +6,15 @@ export const Container = styled.div`
   align-items: center;
   height: 100%;
 `;
+
+export const CarouselContainer = styled.div`
+  height: 244px;
+  width: 610px;
+
+  img {
+    height: 244px;
+    width: 550px;
+    margin-left: 30px;
+    border-radius: 20px;
+  }
+`;
